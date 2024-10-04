6c55849b31cf32728f4a199b8e10c7e2a4802bf7
Анна <vindanka@gmail.com>
2024-10-04 14:57:33 +0300

    Overload operators and test if it is working

--- ArythmeticOverload/ComplexNumber.cs
+++ ArythmeticOverload/ComplexNumber.cs
@@ -10,5 +10,38 @@
             this.imaginary = imaginary;
         }
 
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.real + b.real, a.imaginary + b.imaginary);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.real - b.real, a.imaginary - b.imaginary);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            double real = a.real * b.real - a.imaginary * b.imaginary;
+            double imaginary = a.real * b.imaginary + a.imaginary * b.real;
+
+            return new ComplexNumber(real, imaginary);
+        }
+
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            double real = (a.real * b.real + a.imaginary * b.imaginary) /
+                (b.real * b.real + b.imaginary * b.imaginary);
+            double imaginary = (a.real * b.imaginary - a.imaginary * b.real) /
+                (b.real * b.real + b.imaginary * b.imaginary);
+
+            return new ComplexNumber(real, -imaginary);
+        }
+
+        public override string ToString()
+        {
+            if (imaginary == 0) return real.ToString();
+            return imaginary > 0 ? $"{real}+{imaginary}i" : $"{real}{imaginary}i";
+        }
     }
 }
--- Task-7/Program.cs
+++ Task-7/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿var num1 = new ArythmeticOverload.ComplexNumber(7, -4);
+var num2 = new ArythmeticOverload.ComplexNumber(3, 2);
+
+Console.WriteLine((num1 / num2).ToString());
+Console.WriteLine((num1 * num2).ToString());
+Console.WriteLine((num1 - num2).ToString());
+Console.WriteLine((num1 + num2).ToString());
