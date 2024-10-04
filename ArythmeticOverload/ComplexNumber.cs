fbd163410c45e881d3c55ef84e49f3d27886e42b
Анна <vindanka@gmail.com>
2024-10-04 14:31:32 +0300

    Create ComplexNumber class

---
+++ ArythmeticOverload/ComplexNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace ArythmeticOverload
+{
+    public class ComplexNumber
+    {
+        private double real, imaginary;
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+    }
+}
